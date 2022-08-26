91981859937ef95668a40518429031ae9336c608
Francisco Sales <salesfrancisco73@gmail.com>
2022-08-26 13:25:30 -0400

    [c6bank] refactoring tests

--- app/src/main/java/com/francisco/strider/data/repostiory/GithubRepositoryImpl.kt
+++ app/src/main/java/com/francisco/strider/data/repostiory/GithubRepositoryImpl.kt
@@ -4,8 +4,6 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.francisco.strider.data.api.GithubRepositoryApi
-import com.francisco.strider.data.entities.Github
-import com.francisco.strider.data.mapper.ItemMapper.toPosts
 import com.francisco.strider.domain.models.Post
 import com.francisco.strider.domain.repository.GithubRepository
 import com.francisco.strider.feature.home.GithubDataSource
